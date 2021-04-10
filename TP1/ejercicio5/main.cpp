301e18779b504326a2b343babce091fe7b2c1775
tomasCiganda <totocigandaazcarate@gmail.com>
2021-04-10 13:29:03 -0300

    ejercicio 5 interes compuesto

--- TP1/ejercicio5/main.cpp
+++ TP1/ejercicio5/main.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
+double rentabilidadEstimada(int periodoMeses, double montoInicial, double interesAnual){
+	//Aplico formula ded interes compuesto montoInicial*(1+interes/100/12)^meses - monto inicial
+	double rentabilidad = (montoInicial*(pow((1+((interesAnual/100)/12)), periodoMeses)))-montoInicial;
+
+	return rentabilidad;
+
+}
+
 int main (){
 
+	int periodoMeses;
+	double montoInicial;
+	double interesAnual;
+
+	cout << "Ingrese el monto anual que desea ingresae al banco, la tasa de interes anual y el periodo te tiempo en meses" << endl;
+
+	cin >> montoInicial >> interesAnual >> periodoMeses;
+	cout << "La rentabilidad es de: " << rentabilidadEstimada(periodoMeses, montoInicial, interesAnual) << endl;
 }
