7b3ae069a28b4b1fab57a7d00a48c484d43ed002
tomasCiganda <totocigandaazcarate@gmail.com>
2021-05-01 13:54:03 -0300

    solucion eliminacion de lista simple

--- listaSimple/main.cpp
+++ listaSimple/main.cpp
@@ -8,37 +8,81 @@ struct nodo
 	nodo * siguiente;
 };
 
-nodo * crearNodo();
+nodo * crearNodo(int elemento, nodo * siguiente);
 
-void agregarLista(nodo * primero, nodo * nodoAgregar);
+nodo * agregarElemento(nodo * primero, int nuevoElemento);
 
-void eliminarLista();
+bool eliminarLista(nodo * &primero, int elementoAEliminar);
 
-void mostrarLista();
+void mostrarLista(nodo * primero);
 
 int main ()
 {
-	cout << "inicio de ejecucion";
+	cout << "inicio de ejecucion" << endl;
 
-	nodo * lista = new nodo;
-
+	nodo * lista = crearNodo(0, NULL);
+	lista = agregarElemento(lista, 1);
+	lista = agregarElemento(lista, 2);
+	mostrarLista(lista);
+	if(eliminarLista(lista, 9))
+	{
+		cout << "se borro el elemento" << endl;
+	}
+	else
+	{
+		cout << "no se borro el elemento" << endl;
+	}
+	cout << "fin de la eliminacion" << endl;
+	mostrarLista(lista);
 	return 0;
 }
 
-nodo * crearNodo(int elemento)
+nodo * crearNodo(int elemento, nodo * nodoSiguiente)
 {
-	nodo * nuevoNodo = new nodo;
+	nodo * nuevoNodo = new nodo();
 	nuevoNodo->elemento = elemento;
+	nuevoNodo->siguiente = nodoSiguiente;
 
 	return nuevoNodo;
 }
 
-void agregarLista(nodo * primero, nodo * nodoAgregar)
+nodo * agregarElemento(nodo * primero, int nuevoElemento)
 {
-	while (primero->siguiente != NULL)
+	nodo * nuevoNodo = crearNodo(nuevoElemento, primero);
+
+	return nuevoNodo;
+}
+
+void mostrarLista(nodo * primero)
+{
+	nodo * cursor = primero;
+
+	while (cursor != NULL)
 	{
-		primero-> siguiente = primero->siguiente->siguiente;
+		cout << "Valor de nodo: " << cursor->elemento << endl;
+
+		cursor = cursor->siguiente; 
+
+	}
+}
+
+bool eliminarLista(nodo * &primero, int elementoAEliminar)
+{
+	nodo ** cursor = &primero;
+	while ((*cursor) != NULL)
+	{
+		if ((*cursor)->elemento == elementoAEliminar)
+		{
+			nodo * borrar = *cursor;
+			*cursor = (*cursor)->siguiente;
+			delete borrar;
+			return true;
+		}
+		else
+		{
+			cursor = &(*cursor)->siguiente;
+		}
 	}
 
-	primero->siguiente = nodoAgregar;
+	return false;
 }
